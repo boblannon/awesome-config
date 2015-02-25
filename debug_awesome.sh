a30006efbf1886399365f68be42b8964e595f3c7
Bob Lannon <blannon@gmail.com>
2015-02-25 01:45:41 -0500

    better debugging, kill pid when quitting

--- debug_awesome.sh
+++ debug_awesome.sh
@@ -2,5 +2,7 @@
 # script xepyhr-awesome
 # author: dante4d <dante4d@gmail.com>
 Xephyr -ac -br -noreset -screen 800x600 :1 &
+ZEPHYR_PID=$!
 sleep 1
 DISPLAY=:1.0 awesome -c ~/.config/awesome/rc.lua.new
+kill $ZEPHYR_PID
